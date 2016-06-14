1834021bf04b7cc7417f6ba39466cfc69d4843a4
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2016-06-14 23:49:23 +0300

    add on delete cascade for delete operation_history

--- src/main/resources/sql_script/mysql_procedure_moz_20160609.sql
+++ src/main/resources/sql_script/mysql_procedure_moz_20160609.sql
@@ -10,7 +10,7 @@ CREATE TABLE operation_history_moz(
     procedure_moz_id INT NOT NULL,
     UNIQUE (operation_history_moz_id),
     FOREIGN KEY (operation_history_moz_id)
-            REFERENCES operation_history(operation_history_id),
+            REFERENCES operation_history(operation_history_id) ON DELETE CASCADE ,
     FOREIGN KEY (procedure_moz_id)
             REFERENCES procedure_moz(procedure_moz_id)
 );
@@ -19,3 +19,14 @@ INSERT INTO operation_history_moz SELECT operation_history_id, procedure_id FROM
 
 ALTER TABLE operation_history DROP FOREIGN KEY procedure_id;
 ALTER TABLE operation_history DROP COLUMN procedure_id;
+
+-- not notig
+ALTER TABLE `operation_history_moz` DROP FOREIGN KEY `operation_history_moz_ibfk_1`;
+
+ALTER TABLE `operation_history_moz` ADD CONSTRAINT `operation_history_moz_ibfk_1` FOREIGN KEY (`operation_history_moz_id`) REFERENCES `operation_history`(
+    `operation_history_id`
+) 
+    ON DELETE CASCADE 
+            ON UPDATE
+                RESTRICT;
+
